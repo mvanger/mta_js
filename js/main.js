2d2a9b693aa118821df9f28110e4daec39054157
mvanger <mvanger@nyu.edu>
2013-07-23 09:49:30 -0400

    Made some arrays for the subway lines

---
+++ js/main.js
@@ -0,0 +1,18 @@
+n_line = ['Times Square',
+          '34th',
+          '28th',
+          '23rd',
+          'Union Square',
+          '8th'];
+l_line = ['8th Ave',
+          '6th Ave',
+          'Union Square',
+          '3rd Ave',
+          '1st Ave'];
+six_line = ['Grand Central',
+            '33rd',
+            '28th',
+            '23rd',
+            'Union Square',
+            'Astor Place'];
+
