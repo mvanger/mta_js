39c1c30ff91eb2a0751ae93a74a1a3dcb613c43f
mvanger <mvanger@nyu.edu>
2013-07-23 09:51:36 -0400

    Made a hash or object thing

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-n_line = ['Times Square',
+var n_line = ['Times Square',
           '34th',
           '28th',
           '23rd',
           'Union Square',
           '8th'];
-l_line = ['8th Ave',
+var l_line = ['8th Ave',
           '6th Ave',
           'Union Square',
           '3rd Ave',
           '1st Ave'];
-six_line = ['Grand Central',
+var six_line = ['Grand Central',
             '33rd',
             '28th',
             '23rd',
             'Union Square',
             'Astor Place'];
 
+var mta = {
+  n: n_line,
+  l: l_line,
+  six: six_line
+};
